68a1a233aa38509a026c9a6e75e95f55436e61ce
unknown <peng.707@buckeyemail.osu.edu>
2026-01-24 09:50:45 -0500

    u

--- data.ts
+++ data.ts
@@ -30,15 +30,7 @@ const createSutraVolumes = (
 
 // Based on the verified SubID list for the Prajna section (id/43)
 const prajna: Volume[] = [
-  ...createSutraVolumes(1000, 67, 1, '大般若波羅蜜多j', 600),
-  ...createSutraVolumes(2000, 68, 2, '放光般若波羅蜜j', 30),
-  ...createSutraVolumes(3000, 69, 3, '摩訶般若波羅蜜j', 30),
-  ...createSutraVolumes(4000, 70, 4, '光讚般若波羅蜜j', 10),
-  ...createSutraVolumes(5000, 71, 5, '道行般若波羅蜜j', 10),
-  ...createSutraVolumes(6000, 75, 6, '小品般若波羅蜜j', 10),
-  ...createSutraVolumes(7000, 76, 7, '摩訶般若波羅蜜鈔j', 5),
-  ...createSutraVolumes(8000, 80, 9, '勝天王般若波羅蜜j', 7),
-  ...createSutraVolumes(9000, 90, 19, '文殊師利所說般若波羅蜜j', 1),
+  ...createSutraVolumes(1000, 67, 1, '大般若波羅蜜多j', 300),
 ];
 
 export const INITIAL_VOLUMES: Volume[] = prajna;
